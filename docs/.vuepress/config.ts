5255dbc611c73c9d90a595c4a2e8c880d4fc208b
wangcheng <wangcheng@yaofangwang.com>
2025-03-19 16:33:52 +0800

    test: 自动化部署

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,13 +3,12 @@ import { defineUserConfig } from 'vuepress'
 import theme from './theme'
 
 export default defineUserConfig({
-  base: '/wang-sepc/',
+  base: '/',
 
   lang: 'en-US',
   title: 'wang-sepc',
   description: '团队自定义规范，一键扫描，一键修复，一键搭建',
 
  theme,
-
  
 })
