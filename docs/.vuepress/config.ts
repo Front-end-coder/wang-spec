aa38efb6dd9dece687cce13f5e106797c0a43bbc
wangcheng <wangcheng@yaofangwang.com>
2025-03-20 09:06:15 +0800

    fix: 修改.vuepress配置项

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,14 +1,15 @@
  
 import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
 import theme from './theme'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/wang-spec/',
 
   lang: 'en-US',
   title: 'wang-sepc',
   description: '团队自定义规范，一键扫描，一键修复，一键搭建',
-
+ 
  theme,
  
 })
