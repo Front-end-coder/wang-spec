a0cff11abd78f85db48b2e04ab15f4f613da9c88
wangcheng <1451430337@qq.com>
2025-03-23 11:12:38 +0800

    feat: 完成eslint自定义插件

---
+++ packages/eslint-plugin/configs/recommeded.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: ['wang-eslint-plugin'],
+    rules: {
+        'wang-eslint-plugin/no-http-url': 'warn',
+        'wang-eslint-plugin/no-secret-info': 'error',
+    }
+}
