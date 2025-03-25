d236769f6b4b6de15dfa24c644f114be69075b48
wangcheng <wangcheng@yaofangwang.com>
2025-03-25 11:55:48 +0800

    feat: 修复评论和搜索功能

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -18,14 +18,14 @@ export default hopeTheme({
         // 评论区
         comment: {
             provider: "Giscus",
-            repo: "https://github.com/Front-end-coder/wang-spec",
+            repo: "Front-end-coder/wang-spec",
             repoId: "R_kgDOOJsd5A",
             category: "Announcements",
             categoryId: "DIC_kwDOOJsd5M4CoKFK",
           },
         // 搜索
         slimsearch: {
-            suggestion: true,
+            autoSuggestions: true,
         }
     }
 })
